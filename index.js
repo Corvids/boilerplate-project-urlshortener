3fca59e385f9e16bc47d412afbc06e7daa40fbe0
Jennifer <joonnifer@gmail.com>
2023-05-20 12:51:34 -0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,6 +20,31 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Mount body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+function isValidURL(string) {
+  var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  return (res !== null)
+};
+
+
+app.post("/api/shorturl", function(req, res) {
+  console.log(req.body.url)
+  const url = req.body.url
+  
+  if (!isValidURL(url)) {
+    res.json({
+      error: "invalid url",
+    });
+  } else {
+    res.json({
+      original_url: url,
+      short_url: 1
+    });
+  }
 });
